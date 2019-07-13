770b225c0c96a03a54f698d53d25529146b43f9d
prasetyo-adi <52805350+prasetyo-adi@users.noreply.github.com>
2019-07-13 22:52:14 +0700

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,34 @@
+function getProfile() {
+return{
+ name: "Prasetyo Adi Wibowo",
+ age: 19,
+ address: "jalan kruwing,desa tirta kencana,kecamatan rimbo bujang, kabupaten tebo, provinsi jambi",
+ hobbies: "coding dan bermain game",
+ is_married: false,
+ list_school[{
+ 	high_school:"SMAN2 kabupaten Tebo",
+ 	year_in:2016,
+ 	year_out:2019,
+ 	major:"sains",
+ 	university:""
+ }]
+
+ skills:[{
+ 	skill_name:"web design with html and css",
+ 	level:"beginner"
+ }]
+
+ interest_in_coding: true;
+
+ }
+
+
+}
+
+
+
+
+
+
+
+
